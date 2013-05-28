1f6d79f5f05c169c2718dfb114db9c5398e6ef6a
kai23 <kai_23_@hotmail.Com>
2013-05-28 14:20:28 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,7 +4,7 @@
 apt-get update -y && apt-get upgrade -y
 
 # On installe tout ce dont on va avoir besoin
-apt-get install -y apache2 apache2-doc apache2-mpm-prefork apache2-utils libexpat1 ssl-cert libapache2-mod-php5 php5 php5-common php5-curl php5-dev php5-gd php5-idn php-pear php5-imagick php5-imap php5-json php5-mcrypt php5-memcache php5-mhash php5-ming php5-mysql php5-ps php5-pspell php5-recode php5-snmp php5-sqlite php5-tidy php5-xmlrpc php5-xsl libapache2-mod-scgi build-essential make gcc autoconf curl libcurl3 libcurl4-openssl-dev  zip unzip gcc libc6-dev linux-kernel-headers diff wget bzip2 make screen ffmpeg libcppunit-dev libncurses5-dev libncursesw5-dev subversion libsigc++ dtach imagemagick proftpd zsh git
+apt-get install -y apache2 apache2-doc apache2-mpm-prefork apache2-utils libexpat1 ssl-cert libapache2-mod-php5 php5 php5-common php5-curl php5-dev php5-gd php5-idn php-pear php5-imagick php5-imap php5-json php5-mcrypt php5-memcache php5-mhash php5-ming php5-mysql php5-ps php5-pspell php5-recode php5-snmp php5-sqlite php5-tidy php5-xmlrpc php5-xsl libapache2-mod-scgi build-essential make gcc autoconf curl libcurl3 libcurl4-openssl-dev  zip unzip gcc libc6-dev linux-kernel-headers diffutils wget bzip2 make screen ffmpeg libcppunit-dev libncurses5-dev libncursesw5-dev subversion libsigc++ dtach imagemagick proftpd zsh git
 
 
 
